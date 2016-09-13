c4e9a68a1c567b0292ddb2c8999522468458eedc
Patryk Scholtysek <patryk@scholtysek-software.com>
2016-09-13 12:47:56 +0200

    Add mysql container
    Added container with mysql:5.7 database.
    Created Test entity to check configuration.

---
+++ src/AppBundle/Entity/Test.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="test")
+ */
+class Test
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
